7451d8fa39fbabfcbb0044b02dc70a23d19671b0
Anna Förster <anna.foerster@elkb.de>
2020-09-01 13:18:36 +0200

    update

--- startseite_script_no_slider.js
+++ startseite_script_no_slider.js
@@ -1,3 +1,4 @@
+//show more headlines
 $(function(){
 	$("#headlines-plus-minus").click(function(){
 		$(".second-row-link").toggleClass("visible");
@@ -9,6 +10,24 @@ $(function(){
 			$("#headlines-plus-minus").removeClass("fa-minus");
 			$("#headlines-plus-minus").addClass("fa-plus");
 			$('#headlines-plus-minus').prop('title', 'mehr Schlagzeilen anzeigen');
+		}
+	});
+});
+
+//hide and show the headlines
+$(function(){
+	$(".elkb-schlagzeilen-ueberschrift-no-slider").click(function(){
+		$(".headlines-no-slider-all").toggleClass("invisible");
+		$(".fa-caret-down").toggleClass("rotate270");
+		if ($(".headlines-no-slider-all").hasClass("invisible")) {
+			if ($(".second-row-link").hasClass("visible")) {
+				$(".second-row-link").removeClass("visible");
+				$("#headlines-plus-minus").removeClass("fa-minus");
+				$("#headlines-plus-minus").addClass("fa-plus");
+			}
+			$(".fa-plus").css('display', 'none');
+		} else{
+			$(".fa-plus").css('display', 'inline');
 		}
 	});
 });
